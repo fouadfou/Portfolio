c580cd7baf7561ab4dd183df05b92ef5c16e2dd5
fouadfou <fou33fou@gmail.com>
2023-10-26 12:44:50 +0100

    first commit

--- client/src/sections/Contact.jsx
+++ client/src/sections/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState } from "react";
 import img from "../assets/images/shaking hands.png";
 import axios from "axios";
 
@@ -25,7 +25,7 @@ const Contact = ({ sectionRef }) => {
 
     try {
       const response = await axios.post(
-        "https://ounadi.onrender.com/send_mail",
+        "https://ouunadi.onrender.com/send_mail",
         formData
       );
       console.log(response.data.message);
