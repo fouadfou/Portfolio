8c28c38c19d549cb2f2b0caca8b9694642a0c6c2
fouadfou <fou33fou@gmail.com>
2023-10-26 12:45:45 +0100

    first commit

--- client/src/sections/Contact.jsx
+++ client/src/sections/Contact.jsx
@@ -25,7 +25,7 @@ const Contact = ({ sectionRef }) => {
 
     try {
       const response = await axios.post(
-        "https://ouunadi.onrender.com/send_mail",
+        "https://ounadi.onrender.com/send_mail",
         formData
       );
       console.log(response.data.message);
