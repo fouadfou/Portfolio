b4b1a6fdfb634a27cb0e24e4c05a0d3bd3851d01
fouadfou <fou33fou@gmail.com>
2023-10-29 11:00:47 +0100

    more responsive

--- client/src/components/HeaderContainer.jsx
+++ client/src/components/HeaderContainer.jsx
@@ -33,19 +33,19 @@ const HeaderContainer = () => {
   }, []);
 
   return (
-    <div className="container absolute  flex items-center justify-center md:justify-normal min-w-full  h-screen top-0" style={{backgroundImage:`url(${bg})`, backgroundPosition:"center" ,backgroundSize:"cover"}}>
+    <div className="container absolute  flex items-center justify-center md:justify-normal min-w-full   top-0" style={{backgroundImage:`url(${bg})`, backgroundPosition:"center" ,backgroundSize:"cover"}}>
       
         <div className="sub-container  z-40 w-[80%] md:w-[45%] min-w-[250px]  flex flex-col items-center md:items-baseline 
-          gap-14 mx-[7%] mt-[1.5rem] mb-[3.5rem] md:mb-[1.2rem] ">
-          <h1 className="relative  font-Ginerin text-[4rem] md:text-[5.8rem] text-white select-none">
+            mx-[7%] mt-[1.5rem] mb-[3.5rem] md:mb-[1.2rem] ">
+          <h1 className="relative  font-Ginerin text-[4rem] lg:text-[5.8rem] text-white select-none">
             Fouad
             <sub
-              className="absolute  top-[4.6rem] left-[3.8rem]  md:top-[6.5rem] md:left-[5.65rem] font-Wind font-bold
-                        text-[#FFBF00] text-[5rem]  md:text-[7.1rem]">
+              className="absolute  top-[4.6rem] left-[3.8rem]  lg:top-[6.5rem] lg:left-[5.65rem] font-Wind font-bold
+                        text-[#FFBF00] text-[5rem]  lg:text-[7.1rem]">
               Ounadi
             </sub>
           </h1>
-          <p className="text-[#e7e7e789]  flex-wrap text-center md:text-left select-none">
+          <p className="text-[#e7e7e789] md:my-10 my-[15%] flex-wrap text-center md:text-left select-none">
           Specializing in web development, I'm committed to creating high-performance websites 
           that prioritize a seamless UI/UX design. My expertise lies in merging technical excellence with captivating 
           user experiences to deliver top-tier websites.
